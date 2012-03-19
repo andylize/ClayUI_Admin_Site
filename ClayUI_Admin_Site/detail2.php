b0d06d901dc6be224ebd255ce16303a8327b112c
Andy Lize <alize@netinfocentral.com>
2012-03-19 05:59:41 -0500

    Added menu screens and logic for admin site.

---
+++ ClayUI_Admin_Site/detail2.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<style>
+		body 
+		{
+			background:#EBEBEB;
+			margin:0;
+			padding:0;
+			font:12px verdana, sans-serif;
+			color:#000;
+		}
+	</style>
+</head>
+<body>
+<?php
+	echo "This is the detail 2 area";
+ ?>
+ </body>
+ </html>
+
---
+++ ClayUI_Admin_Site/menu.php
@@ -0,0 +1,108 @@
+<html>
+<head>
+	<link href="./stylesheets/admin.css" media="screen" rel="stylesheet" type="text/css" />
+<?php 
+	$username = $_SERVER['CLAYUI_USER'];
+	$password = $_SERVER['CLAYUI_PASS'];
+	$database = $_SERVER['CLAYUI_DB'];
+	//$sql = "SELECT ApplicationID, ApplicationName FROM Applications ORDER BY ApplicationName ASC;";
+	$sql = "CALL uspGetAppMenuStructure();";
+	
+	mysql_connect(localhost, $username, $password);
+	mysql_select_db($database) or die("Unable to select database");
+	$result = mysql_query($sql);
+	
+?>
+</head>
+<body class="menu">
+<div style="background-color: #707070; border-style: solid; border-width: 1px; padding-top: 5px; padding-bottom: 5px;">Application Tree<a href="javascript:history.go(0)">Add New..></a></div>
+	<?php
+		$currentPosition = 1;
+		$lastPosition = 1;
+		while($row = mysql_fetch_array($result, MYSQL_NUM))
+		{
+			// get current position
+			$currentPosition = $row[1];
+			
+			// check to see if this is an element or data table
+			if($row[4] >= 4)
+			{
+				// if current position is less than the last position then we've moved up a level and need to close the ordered list
+				if($currentPosition < $lastPosition)
+				{
+					printf('</ol>');
+				}
+				
+				if($row[4] == 4)
+				{
+					printf('<li class="element"><a href="element.php?appID=%s&appPartID=%s&elementID=%s" target="detail">%s</a></li>', $row[0], $row[3], $row[6], $row[5]);
+				}
+				if($row[4] == 5)
+				{
+					printf('<li class="dataTable"><a href="dataTable.php?appID=%s&appPartID=%s" target="detail">%s</a>', $row[0], $row[3], $row[5]);
+				}
+			}
+			else 
+			{			
+				// if current position is less than the last position then we've moved up a level and need to close the ordered list
+				if($currentPosition < $lastPosition)
+				{
+					printf('</li></ol>');
+				}
+				
+				// check if this is a new app
+				if($row[4] == 1)
+				{
+					// if last position is > 1 then we'll need to close the ordered lists
+					for($x = $lastPosition; $x > 0 ; $x--)
+					{
+						if($x > 1)
+						{
+							printf('</li></ol>');
+						}
+						else
+						{
+							printf('</ol>');
+						}
+					}
+					
+					printf('<ol class="tree">');
+					printf('<li>');
+					printf('<label for="%s" class="application"><a href="ApplicationDetail.php?appID=%s" target="detail">%s</a></label> <input type="checkbox" id="%s" />', $row[5], $row[0], $row[5], $row[5]);
+					printf('<ol>');
+					printf('<li>');
+				}
+				// check if this is a AppPart
+				if($row[4] == 2)
+				{
+					printf('<label for="%s" class="appPart"><a href="AppPartDetail.php?appID=%s&appPartID=%s" target="detail">%s</a></label> <input type="checkbox" id="%s" />', $row[5], $row[0], $row[3], $row[5], $row[5]);
+					printf('<ol>');
+					printf('<li>');
+				}
+				// check if this is an Elements
+				if($row[4] == 3)
+				{
+					printf('<label for="%s" class="elements"><a href="ElementsDetail.php?appID=%s&appPartID=%s" target="detail">%s</a></label> <input type="checkbox" id="%s%s" />', $row[5], $row[0], $row[3], $row[5], $row[5], $row[3]);
+					printf('<ol>');
+				}
+			}			
+			// move positions
+			$lastPosition = $currentPosition;
+		}
+		// close out ordered lists
+		for($x = $lastPosition; $x > 0 ; $x--)
+		{
+			if($x > 1)
+			{
+				printf('</li></ol>');
+			}
+			else
+			{
+				printf('</ol>');
+			}
+		}
+	?> 
+<?php mysql_close();?>
+ </body>
+ </html>
+
